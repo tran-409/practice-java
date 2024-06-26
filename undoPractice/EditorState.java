a1433d6a7e1f3deee0123ce19e7c9f5567933194
Tu Tran <95881666+tran-409@users.noreply.github.com>
2024-06-26 13:05:18 -0500

    Add files via upload

---
+++ undoPractice/EditorState.java
@@ -0,0 +1,16 @@
+package undoPractice;
+
+public class EditorState {
+	
+	private final String content;
+
+	public EditorState(String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+	
+	
+}
